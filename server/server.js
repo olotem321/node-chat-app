9213e8a7f03ecec12b0f2d8b29f523f59a6bef64
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-20 16:36:12 +0700

    Add connect and disconnect event handle

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -7,10 +9,28 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+var currentUser = 0;
+
+io.on('connection', (socket) => {
+  currentUser++;
+  console.log('new user connected!');
+  console.log('Current user: ',currentUser);
+
+  socket.on('disconnect', () => {
+    currentUser--;
+    console.log('User disconnect');
+    console.log('Current user: ',currentUser);
+  });
+
+});
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`Start on port ${port}`);
-})
+});
